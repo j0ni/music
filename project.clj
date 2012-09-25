a28f00fbd3685c1535a260f0c32361a5306879e4
J Irving <j@lollyshouse.net>
2012-09-24 22:00:06 -0400

    First tutorial

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject music "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "Overtone Playground"
+  :url "http://j0ni.ca"
+  :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License"
+            :url "http://creativecommons.org/licenses/by-sa/3.0/"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.7.1"]])
--- src/music/core.clj
+++ src/music/core.clj
@@ -1,5 +1,43 @@
 (ns music.core)
 
+
+(use 'overtone.live)
+
+(definst foo [] (saw 220))
+
+(foo)
+(kill 4)
+(kill foo)
+
+(definst bar [freq 220] (saw freq))
+(bar 110)
+(kill bar)
+
+(definst baz [freq 440] (* 0.3 (saw freq)))
+(baz 220)
+(baz 660)
+(kill baz)
+
+
+(definst quux [freq 440] (* 0.3 (saw freq)))
+(quux)
+(ctl quux :freq 660)
+
+
+(definst trem [freq 440 depth 10 rate 6 length 3]
+  (* 0.3
+     (line:kr 0 1 length FREE)
+     (saw (+ freq (* depth (sin-osc:kr rate))))))
+
+(trem)
+(trem 200 60 0.8)
+(trem 60 30 0.2)
+(stop)
+
+(kick)
+
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
